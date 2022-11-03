32a690d93a0496641d79871e08a91a7d20cc0069
Eyuel <eyueltb@gmail.com>
2022-11-03 15:59:34 +0100

    DataStructure and algorithm started

--- schoolProject/src/main/java/lnu/ds/Bags/IBag.java
+++ schoolProject/src/main/java/lnu/ds/Bags/IBag.java
@@ -2,48 +2,30 @@ package lnu.ds.Bags;
 
 interface IBag<T> {
 
-    /**
-     * Gets the current number of entries in this bag.
-     */
+    /** Gets the current number of entries in this bag. */
     int getCurrentSize();
 
-    /**
-     * Sees whether this bag is empty. Return True if the bag is empty, or false if not.
-     */
+    /** Sees whether this bag is empty. Return True if the bag is empty, false otherwise */
     boolean isEmpty();
 
-    /**
-     * Adds a new entry to this bag. Return True if the addition is successful, or false if not.
-     */
+    /** Adds a new entry to this bag. Return True if the added successful, or false otherwise */
     boolean add(T newEntry);
 
-    /**
-     * Removes one unspecified entry from this bag, if possible. Return Either the removed entry, or null if not successful, or null.
-     */
+    /** Removes one unspecified entry from this bag, if possible. Return Either the removed entry, or null if not successful, or null.*/
     T remove();
 
-    /**
-     * Removes one occurrence of a given entry from this bag, if possible. Return True if the removal was successful, or false if not.
-     */
+    /** Removes one occurrence of a given entry from this bag, if possible. Return True if successful, false otherwise .*/
     boolean remove(T anEntry);
 
-    /**
-     * Removes all entries from this bag.
-     */
+    /** Removes all entries from this bag. */
     void clear();
 
-    /**
-     * Counts the number of times a given entry appears in this bag. Return The number of times anEntry appears in the bag.
-     */
+    /** Counts the number of times a given entry appears in this bag.*/
     int getFrequencyOf(T anEntry);
 
-    /**
-     * Tests whether this bag contains a given entry.Return True if the bag contains anEntry, or false if not.
-     */
+    /**  Tests whether this bag contains a given entry. Return True if anEntry found, false otherwise */
     boolean contains(T anEntry);
 
-    /**
-     * Retrieves all entries that are in this bag. Return A newly allocated array of all the entries in the bag.
-     */
+    /** Retrieves all entries that are in this bag. Return A newly allocated array of all the entries in the bag.*/
     T[] toArray();
 }
